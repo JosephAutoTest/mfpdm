1f9a5209af6e2caf45acbea406ee65a3bad92fc9
joseph.quansah <joseph.quansah@AMCHLT211.ampcus.corp>
2018-05-08 11:06:51 -0400

    Adding new java code

--- GitHubProject/src/test/java/my/repository/MyGitRepository.java
+++ GitHubProject/src/test/java/my/repository/MyGitRepository.java
@@ -1,9 +1,11 @@
-package my.repository;
+ package my.repository;
 
 public class MyGitRepository {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Learning programing is fun");
 
 	}
 
---
+++ GitHubProject/src/test/java/my/repository/TestRepository.java
@@ -0,0 +1,5 @@
+package my.repository;
+
+public class TestRepository {
+
+}
